b8c40c964194bb96ea61930a625d302e2e003aec
richterwindward <rbrzeski19@winwdardschool.org>
2018-09-25 10:06:46 -0700

    add basic functionality to Card class

--- src/sample/Card.java
+++ src/sample/Card.java
@@ -1,8 +1,16 @@
 package sample;
 
 public class Card {
+    final static char[] suits = {2, 3 , 4, 5, 6 , 7 , 8, 9, 10, 11, 12, 13, 14};
 
-    public Card() {
+    private char suit;
 
+    public Card(char suit) {
+        this.suit = suit;
     }
+
+    static String toString() {
+        
+    }
+
 }
