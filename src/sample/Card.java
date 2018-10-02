217f48bf168bd350c8faace30620697f4360c6db
richterwindward <rbrzeski19@windwardschool.org>
2018-10-02 10:05:19 -0700

    Add card to URL function

--- src/sample/Card.java
+++ src/sample/Card.java
@@ -4,6 +4,10 @@ import javafx.scene.image.Image;
 
 public class Card implements Comparable<Card> {
     final static char[] suits = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+    final static String clubsSuffix = "_of_clubs.png";
+    final static String diamondsSuffix = "_of_diamonds.png";
+    final static String heartsSuffix = "_of_hearts.png";
+    final static String spadesSuffix = "_of_spades.png";
 
     public Image cardImage;
 
@@ -12,8 +16,45 @@ public class Card implements Comparable<Card> {
     public Card(char suit) {
         this.suit = suit;
         switch (this.suit) {
-            case 2:
-                this.cardImage = new Image("../../PNG-cards-1.3/");
+
+        }
+    }
+
+    private String[] toURL() {
+        if(suit >= 2 && suit <= 10) {
+            String prefix = String.valueOf(suit);
+            String[] res = {
+                    prefix + clubsSuffix,
+                    prefix + diamondsSuffix,
+                    prefix + heartsSuffix,
+                    prefix + spadesSuffix
+            };
+            return res;
+        } else {
+            String prefix = "*";
+            switch (suit) {
+                case 11:
+                    // jack
+                    prefix = "jack";
+                case 12:
+                    // queen
+                    prefix = "queen";
+                case 13:
+                    // king
+                    prefix = "king";
+                case 14:
+                    // ace
+                    prefix = "ace";
+            }
+
+            String[] res = {
+                    prefix + clubsSuffix,
+                    prefix + diamondsSuffix,
+                    prefix + heartsSuffix,
+                    prefix + spadesSuffix
+            };
+            
+            return res;
         }
     }
 
